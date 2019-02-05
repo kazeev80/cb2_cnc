48658e1871d492d5002841a06c7aad4cd96a2dfc
kazeev80 <44981352+kazeev80@users.noreply.github.com>
2019-02-06 01:42:54 +0300

    Update instal_linux_cnc.sh

--- instal_linux_cnc.sh
+++ instal_linux_cnc.sh
@@ -1,1 +1,29 @@
-#
+COLOR_NORMAL=$(echo -e "\033[m")
+COLOR_BLUE=$(echo -e "\033[36m")
+COLOR_GREEN=$(echo -e "\033[32m")
+COLOR_YELLOW=$(echo -e "\033[33m")
+COLOR_GRAY=$(echo -e "\033[37m")
+COLOR_RED=$(echo -e "\033[31m")
+
+echoBlue(){
+        echo "${COLOR_BLUE}${1}${COLOR_NORMAL}"
+}
+
+echoRed(){
+        echo "${COLOR_RED}${1}${COLOR_NORMAL}"
+}
+
+echoYellow(){
+        echo "${COLOR_YELLOW}${1}${COLOR_NORMAL}"
+}
+
+echoGreen(){
+        echo "${COLOR_GREEN}${1}${COLOR_NORMAL}"
+}
+
+
+echoYellow "###################################################################"
+echoGreen "set time via NTP"
+sudo ntpdate-debian
+
+
