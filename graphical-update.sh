bef013193c33e4589f9bd9bf6be6b22339bfaa39
Evgeniy Kazeev <kazeev@mail.ru>
2019-02-07 01:32:08 +0300

    add mali drivers

--- graphical-update.sh
+++ graphical-update.sh
@@ -1,3 +1,4 @@
+rm -r xf86-video-fbturbo 
 git clone https://github.com/ssvb/xf86-video-fbturbo
 cd xf86-video-fbturbo
 sudo apt-get install git build-essential xorg-dev xutils-dev x11proto-dri2-dev
@@ -7,3 +8,5 @@ autoreconf -vi
 make
 sudo make install
 sudo cp xorg.conf /etc/X11/xorg.conf
+cd ..
+rm -r xf86-video-fbturbo
