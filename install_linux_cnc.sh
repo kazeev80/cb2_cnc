c50259f9d8e54767efec458a1636be08d7f1b666
Evgeniy Kazeev <kazeev@mail.ru>
2019-02-07 01:04:53 +0300

    temp

--- install_linux_cnc.sh
+++ install_linux_cnc.sh
@@ -98,5 +98,19 @@ sudo apt-get autoremove -y -f
 sudo apt-get clean
 sudo apt-get update -y
 
+
+echoYellow "###################################################################"
+echoGreen "  Update mali video drivers"
+sudo rm -r xf86-video-fbturbo
+git clone https://github.com/ssvb/xf86-video-fbturbo
+cd xf86-video-fbturbo
+sudo apt-get install git build-essential xorg-dev xutils-dev x11proto-dri2-dev
+sudo apt-get install libltdl-dev libtool automake libdrm-dev
+autoreconf -vi
+./configure --prefix=/usr
+sudo make install
+sudo cp xorg.conf /etc/X11/xorg.conf
+cd ..
+
 echoYellow "###################################################################"
 echoGreen "  Installing linux cnc...."
