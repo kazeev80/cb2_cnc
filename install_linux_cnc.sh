82b7863b30f3e4dc1f6ba87fa4d53254019f85ce
kazeev80 <44981352+kazeev80@users.noreply.github.com>
2019-02-07 23:02:49 +0300

    Update install_linux_cnc.sh

--- install_linux_cnc.sh
+++ install_linux_cnc.sh
@@ -66,7 +66,22 @@ sudo aptitude install -y \
     pkg-config autotools-dev autoconf automake cmake \
     uuid-dev libpcre3-dev libsodium-dev valgrind
     
+git clone git://github.com/zeromq/libzmq.git
+cd libzmq
+./autogen.sh
+# do not specify "--with-libsodium" if you prefer to use internal tweetnacl security implementation (recommended for development)
+./configure --with-libsodium
+make check
+sudo make install
+sudo ldconfig
+cd ..
 
+git clone git://github.com/zeromq/czmq.git
+cd czmq
+./autogen.sh && ./configure && make check
+sudo make install
+sudo ldconfig
+cd ..
 
 echoYellow "###################################################################"
 echoGreen "  Installing linux cnc...."
