207341a41dee7fb8bf1349cefa45c00315814f8d
Evgeniy Kazeev <kazeev@mail.ru>
2019-02-08 00:09:20 +0300

    add protobuf

--- install_linux_cnc.sh
+++ install_linux_cnc.sh
@@ -62,7 +62,7 @@ echoYellow "###################################################################"
 echoGreen "Install needed packages"
 sudo aptitude install -y cython
 sudo aptitude install -y uuid-runtime
-sudo aptitude install -y protobuf
+sudo aptitude install -y libprotobuf-dev
 
 sudo aptitude install -y \
     git build-essential libtool \
