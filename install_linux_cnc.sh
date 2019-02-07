3deb55986cfad2da42e6dc16f9220eb01acaa440
Evgeniy Kazeev <kazeev@mail.ru>
2019-02-07 22:39:05 +0300

    temp

--- install_linux_cnc.sh
+++ install_linux_cnc.sh
@@ -38,66 +38,6 @@ echoYellow "###################################################################"
 echoGreen "Upgrade packages"
 sudo apt-get upgrade -y
 
-echoYellow "###################################################################"
-echoGreen "  Install cyrillic"
-sudo bash -c "echo 'style ter-uni-bold'                     >  /etc/console-cyrillic"
-sudo bash -c "echo 'size 16'                               >>  /etc/console-cyrillic"
-sudo bash -c "echo 'encoding utf-8'                        >>  /etc/console-cyrillic"
-sudo bash -c "echo 'layout ru'                             >>  /etc/console-cyrillic"
-sudo bash -c "echo 'options ctrl_shift_toggle win_switch'  >>  /etc/console-cyrillic"
-sudo bash -c "echo 'ttys /dev/tty[1-6]'                    >>  /etc/console-cyrillic"
-
-echoYellow "###################################################################"
-echoGreen "  locale RU.UTF-8"
-
-echo 'VERBOSE_OUTPUT=no
-ACTIVE_CONSOLES="/dev/tty[1-6]"
-
-CHARMAP="UTF-8"
-
-CODESET="guess"
-FONTFACE="TerminusBold"
-FONTSIZE="8x16"
-
-VIDEOMODE=
-
-XKBMODEL="pc105"
-XKBLAYOUT="us,ru"
-XKBVARIANT=",winkeys"
-XKBOPTIONS="grp:ctrl_shift_toggle,grp:rwin_switch,lv3:ralt_switch,grp_led:scroll"
- '> /etc/default/console-setup
-
-echo 'XKBMODEL="pc105"
-XKBLAYOUT="us,ru"
-XKBVARIANT=","
-XKBOPTIONS="grp:ctrl_shift_toggle,grp:rwin_switch,lv3:ralt_switch,grp_led:scroll"
-
-BACKSPACE="guess"'> /etc/default/keyboard
-
-echo 'style ter-uni-framebuf
-size 16
-encoding utf-8
-layout ru
-options ctrl_shift_toggle win_switch
-ttys /dev/tty[1-6]"'> /etc/console-cyrillic
-
-sudo cp /etc/locale.gen  /etc/locale.gen.orig
-sudo bash -c "cat /etc/locale.gen.orig \
-        | sed -e 's/\# ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' > /etc/locale.gen"
-sudo locale-gen
-sudo localedef -i ru_RU -f UTF-8 ru_RU.UTF-8
-sudo apt-get install -y console-cyrillic
-sudo dpkg-reconfigure console-cyrillic
-
-sudo apt-get install -y console-data
-sudo dpkg-reconfigure keyboard-configuration
-
-echoYellow "###################################################################"
-echoGreen "  Update sources"
-sudo apt-get autoremove -y -f
-sudo apt-get clean
-sudo apt-get update -y
-
 
 echoYellow "###################################################################"
 echoGreen "  Update mali video drivers"
