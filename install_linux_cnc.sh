5701935c364db61a1976d16dd02a1e17e8ef0d2b
Evgeniy Kazeev <kazeev@mail.ru>
2019-02-08 00:24:40 +0300

    add libjansson-dev

--- install_linux_cnc.sh
+++ install_linux_cnc.sh
@@ -66,6 +66,9 @@ sudo aptitude install -y libprotobuf-dev
 sudo aptitude install -y libprotoc-dev
 sudo aptitude install -y protobuf-compiler
 sudo aptitude install -y python-setuptools
+sudo aptitude install -y libjansson-dev
+sudo easy_install pip
+sudo pip install protobuf
 
 sudo aptitude install -y \
     git build-essential libtool \
