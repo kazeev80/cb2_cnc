cd7a7c95eb1699d8db1f39deac1933cd675e6931
kazeev80 <44981352+kazeev80@users.noreply.github.com>
2019-02-08 14:57:27 +0300

    Update console-update.sh

--- console-update.sh
+++ console-update.sh
@@ -22,12 +22,26 @@ echoGreen(){
 }
 
 echoYellow "###################################################################"
-
-echoGreen "set time via NTP"
-sudo ntpdate-debian
-
-echoYellow "###################################################################"
 echoGreen "Remove packeges"
+sudo apt-get -y --force-yes autoremove
+
+echoGreen "Remove ajenti"
+sudo apt-get remove -y --force-yes ajenti
+
+echoGreen "Remove mc"
+sudo apt-get -y remove mc mc-data
+
+echoGreen "Remove python"
+sudo apt-get remove -y python python-dbus python-dbus-dev python-gi python-minimal python-support python2.7 python2.7-minimal
+
+sudo apt-get remove -y gsfonts-x11 libfontenc1 libx11-data \
+		       libxau6  libxdmcp6 libxfont1
+
+sudo apt-get remove -y gsfonts-x11 libx11-6 libx11-xcb1 x11-common x11-utils
+
+echoGreen "Remove java"
+sudo apt-get remove -y --force-yes java-common oracle-java7-jdk oracle-java7-jre
+
 sudo apt-get -y --force-yes autoremove
 sudo apt-get clean
 
@@ -54,6 +68,7 @@ sudo bash -c "echo 'ttys /dev/tty[1-6]'                    >>  /etc/console-cyri
 
 echoYellow "###################################################################"
 echoGreen "  locale RU.UTF-8"
+# http://linuxforum.ru/post/413228
 
 sudo bash -c "sh ./prepare-cubian/script/mykeyboardconf.sh"
 
@@ -67,4 +82,3 @@ sudo dpkg-reconfigure console-cyrillic
 
 sudo apt-get install -y console-data
 sudo dpkg-reconfigure keyboard-configuration
-
