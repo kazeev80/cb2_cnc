e08049aefe4b7da23554f5c5750fec7863d2eecf
root <kazeev@mail.ru>
2019-02-08 20:37:57 +0300

    protobuf-compiler

--- 2.install_linux_cnc.sh
+++ 2.install_linux_cnc.sh
@@ -72,8 +72,7 @@ sudo apt-get install --no-install-recommends devscripts equivs -y
 #sudo aptitude install -y cython
 #sudo aptitude install -y uuid-runtime
 sudo aptitude install -y libprotobuf-dev
-#sudo aptitude install -y libprotoc-dev
-#sudo aptitude install -y protobuf-compiler
+sudo aptitude install -y protobuf-compiler
 #sudo aptitude install -y python-setuptools
 #sudo aptitude install -y libjansson-dev
 #sudo aptitude install -y liburiparser-dev
