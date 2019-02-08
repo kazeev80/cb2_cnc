e60e6ed6699b0332bd61d7bade71eb608a542a43
root <root@cubieboard2.(none)>
2019-02-08 08:34:14 +0100

    temp

--- cubian_update.sh
+++ cubian_update.sh
@@ -40,11 +40,10 @@ echoGreen "  Update to Debian 8"
 
 echoYellow "###################################################################"
 echoGreen "  Update /etc/apt/sources.list"
-sudo bash -c "echo 'deb http://debian.mirror.yandex.ru/debian/ jessie main contrib'              > /etc/apt/sources.list"
-sudo bash -c "echo 'deb http://security.debian.org/ jessie/updates main contrib'                >> /etc/apt/sources.list"
-sudo bash -c "echo 'deb http://debian.mirror.yandex.ru/debian/ jessie-updates main contrib'     >> /etc/apt/sources.list"
-sudo bash -c "echo 'deb http://packages.cubian.org/ wheezy main non-free'                       >> /etc/apt/sources.list"
-sudo bash -c "echo 'deb http://ftp.de.debian.org/debian sid main'                               >> /etc/apt/sources.list"
+sudo bash -c "echo 'deb http://debian.mirror.yandex.ru/debian/ sid jessie main contrib'              > /etc/apt/sources.list"
+sudo bash -c "echo 'deb http://security.debian.org/ jessie/updates sid main contrib'                >> /etc/apt/sources.list"
+sudo bash -c "echo 'deb http://debian.mirror.yandex.ru/debian/ sid jessie-updates main contrib'     >> /etc/apt/sources.list"
+sudo bash -c "echo 'deb http://packages.cubian.org/ wheezy main non-free'                           >> /etc/apt/sources.list"
 sudo apt-get autoremove -y -f
 sudo apt-get clean
 sudo apt-get update -y
